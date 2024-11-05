4b6be3e75116e83ac99515e0ec51726aa45536f9
Edom Belayneh <101343364+edombelayneh@users.noreply.github.com>
2024-11-05 13:26:41 -0500

    First Upload

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://boss-terrier-17.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
---
+++ convex/functions/helpers.ts
@@ -0,0 +1,31 @@
+import {
+  customCtx,
+  customMutation,
+  customQuery,
+} from "convex-helpers/server/customFunctions";
+import { getCurrentUser } from "./user";
+import { mutation, query } from "../_generated/server";
+
+export const authenticatedQuery = customQuery(
+  query,
+  customCtx(async (ctx) => {
+    const user = await getCurrentUser(ctx);
+
+    if (!user) {
+      throw new Error("User is not unauthorizd");
+    }
+    return { user };
+  })
+);
+
+export const authenticatedMutation = customMutation(
+  mutation,
+  customCtx(async (ctx) => {
+    const user = await getCurrentUser(ctx);
+
+    if (!user) {
+      throw new Error("User is not unauthorizd");
+    }
+    return { user };
+  })
+);
